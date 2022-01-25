cf4f297179a4c9414044052687fdd343f879dd67
Lasha Palelashvili <falelashvili@gamil.com>
2022-01-25 04:14:49 +0400

    Updates

---
+++ static/js/20.7079f473.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpvuexy-react-admin-dashboard"]=this["webpackJsonpvuexy-react-admin-dashboard"]||[]).push([[20],{989:function(a,s,d){}}]);
+//# sourceMappingURL=20.7079f473.chunk.js.map
